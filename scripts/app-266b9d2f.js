0b1a778cd021f08d1d6913ca08f6634f28e443c9
MariMax <mma29121983@gmail.com>
2015-01-18 18:35:03 +0300

    update

---
+++ scripts/app-266b9d2f.js
@@ -0,0 +1,1 @@
+!function(){function e(e,t){return(" "+e.className+" ").indexOf(" "+t+" ")>-1}var t=document.getElementsByClassName("switcher")[0],s=document.getElementsByClassName("switcher-bottom")[0];s.addEventListener("click",function(){t.className=e(t,"right")?"switcher left":"switcher right"})}();
